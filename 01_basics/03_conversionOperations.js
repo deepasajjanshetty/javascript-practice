8abd0e984d3ebabbc50549c2292ab33d896df33f
Deepa Sajjanshetty <109755802+deepasajjanshetty@users.noreply.github.com>
2025-03-04 09:30:15 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+let score = 33;
+//let score = "33"; defined that in string 
+let character = "deepu";
+let loggedIn = true;
+let symbol = null;
+let something = "30a"; 
+
+console.log(typeof score);
+console.log(typeof character);
+console.log(typeof loggedIn);
+console.log(typeof symbol);
+
+let valueInNumber = Number(character);
+console.log(typeof valueInNumber);
+console.log(typeof symbol);
+let changeSymbol = Number(symbol);
+console.log(typeof changeSymbol);
+console.log(changeSymbol);
+let changeSomething = Number(something);
+console.log(typeof changeSomething);
+console.log(changeSomething);
