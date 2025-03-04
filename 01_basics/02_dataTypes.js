049ca89ad783aad1e96d54267981903630350e3e
Deepa Sajjanshetty <109755802+deepasajjanshetty@users.noreply.github.com>
2025-03-04 08:35:59 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+let age = 12
+let isLoggedIn = true
+let state;
+
+/*
+Primitive Datatypes
+number => numerical values
+bigint => used to stock and sharemarket
+string => character values
+boolean => True/False values
+null => empty value , standalone value (it is a representation of empty value)
+undefined => not defined any value may be in future we define any value
+symbol => unique 
+
+
+Nonprimitive Datatypes
+object =>
+
+*/
+
+console.log(typeof "raj");
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof isLoggedIn);
+console.log(typeof 2);
+console.log(typeof null); //object type
+console.log(typeof undefined); //undefined type
