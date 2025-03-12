15f03c1426329335f3d6c3c6f479536a6563fd5a
Deepa Sajjanshetty <109755802+deepasajjanshetty@users.noreply.github.com>
2025-03-12 07:17:39 +0000

    number maths function

---
+++ 01_basics/07_numberMaths.js
@@ -0,0 +1,8 @@
+const nuroLogy = 400
+console.log(nuroLogy);
+const some = new Number(100)
+console.log(some);
+console.log(some.toString().length);
+console.log(some.toFixed(3));
+console.log(some);
+console.log(some.toPrecision(2));
