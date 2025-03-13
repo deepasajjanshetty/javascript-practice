bebef7572c83b98119628cb19701019916bdafb3
Deepa Sajjanshetty <109755802+deepasajjanshetty@users.noreply.github.com>
2025-03-13 02:11:22 +0000

    updated Math and Number

--- 01_basics/07_numberMaths.js
+++ 01_basics/07_numberMaths.js
@@ -6,3 +6,26 @@ console.log(some.toString().length);
 console.log(some.toFixed(3));
 console.log(some);
 console.log(some.toPrecision(2));
+
+const hundreads = 1000000
+console.log(hundreads.toLocaleString());
+console.log(hundreads.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++++++++++++++++++Maths +++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-6));
+console.log(Math.round(4.69));
+console.log(Math.ceil(4.69));
+console.log(Math.floor(4.69));
+console.log(Math.min(4,6,9,8));
+console.log(Math.max(4,6,9,8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+const min = 5;
+const max = 10;
+console.log(Math.floor(Math.random() * (max - min + 1))+ min);
+
